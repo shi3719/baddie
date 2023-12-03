4280553d25839a459ee7498f6018887399d60baa
shi3719 <shi3719@naver.com>
2023-12-03 23:57:32 +0900

    doneee

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 const btn = document.querySelector('.btn');
 const cat = document.querySelector('.cat');
 const section1 = document.querySelector('.section1');
+const textbox = document.getElementById("textbox");
 
 var arr = ['shit', 'fuck', 'damn', 'bitch', 'crap', 'piss', 'dick', 'darn', 'cock', 'pussy', 'asshole', 'fag', 'bastard', 'slut', 'douche'];
 
@@ -10,13 +11,11 @@ const letter2 = document.querySelector('.letter2');
 const randumletter = () => {
   const randumNum2 = Math.floor(Math.random() * 14);
   letter1.innerHTML = arr[randumNum2];
-  console.log(randumNum2)
 }
 
 const randumletter2 = () => {
   const randumNum2 = Math.floor(Math.random() * 14);
   letter2.innerHTML = arr[randumNum2];
-  console.log(randumNum2)
 }
 
 const randumimg = () => {
@@ -25,9 +24,14 @@ const randumimg = () => {
 }
 
 btn.addEventListener('click', () =>{
-  document.getElementById("textbox").value='';
-  randumimg();
-  section1.style.opacity = '1';
-  randumletter();
-  randumletter2();
+  if(textbox.value === ''){
+    section1.style.opacity = '1';
+    letter1.innerHTML = '편지써!!!!!!';
+  }else{
+    textbox.value='';
+    randumimg();
+    section1.style.opacity = '1';
+    randumletter();
+    randumletter2();
+  }
 })
