f6c56f31281a3f05b3655a4e4bc6b70a0b2717c8
shi3719 <shi3719@naver.com>
2023-12-03 23:54:33 +0900

    done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,23 @@
 const btn = document.querySelector('.btn');
 const cat = document.querySelector('.cat');
+const section1 = document.querySelector('.section1');
+
+var arr = ['shit', 'fuck', 'damn', 'bitch', 'crap', 'piss', 'dick', 'darn', 'cock', 'pussy', 'asshole', 'fag', 'bastard', 'slut', 'douche'];
+
+const letter1 = document.querySelector('.letter1');
+const letter2 = document.querySelector('.letter2');
+
+const randumletter = () => {
+  const randumNum2 = Math.floor(Math.random() * 14);
+  letter1.innerHTML = arr[randumNum2];
+  console.log(randumNum2)
+}
+
+const randumletter2 = () => {
+  const randumNum2 = Math.floor(Math.random() * 14);
+  letter2.innerHTML = arr[randumNum2];
+  console.log(randumNum2)
+}
 
 const randumimg = () => {
   const randumNum = Math.floor(Math.random() * 8);
@@ -9,4 +27,7 @@ const randumimg = () => {
 btn.addEventListener('click', () =>{
   document.getElementById("textbox").value='';
   randumimg();
+  section1.style.opacity = '1';
+  randumletter();
+  randumletter2();
 })
